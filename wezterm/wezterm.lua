2755a38a5669afd94f9915ee7ea199943483c94d
Matt Washer <github@mwasher.com>
2024-08-02 14:58:29 -0500

    Change wezterm theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,6 @@ config = {
   automatically_reload_config  = true,
   enable_tab_bar = false,
   window_decorations = "RESIZE",
-  color_scheme = 'Everforest Dark (Gogh)',
   color_scheme = "Catppuccin Frappe",
   font = wezterm.font("FiraCode Nerd Font", {weight=450, stretch="Normal", style="Normal"}),
   font_size = 12
