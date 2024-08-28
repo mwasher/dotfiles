7bca7de5fde8b613fb511e5384e2b7da8c5848e4
Matt Washer <accounts@mwasher.com>
2024-08-28 20:09:26 +0000

    Add pyenv to path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,6 +2,10 @@ export EDITOR="nvim"
 
 export PATH=$HOME/.local/bin:$PATH
 
+if [ -d "$HOME/.pyenv" ]; then
+  export PATH=$HOME/.pyenv/bin:$PATH
+fi
+
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
 
