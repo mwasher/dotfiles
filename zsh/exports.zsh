0a67d997d2837fac5de3d33d3022a2ae1e7c8ffc
Matt Washer <accounts@mwasher.com>
2024-08-28 20:20:04 +0000

    Fix pyenv shell integration

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,10 +2,6 @@ export EDITOR="nvim"
 
 export PATH=$HOME/.local/bin:$PATH
 
-if [ -d "$HOME/.pyenv" ]; then
-  export PATH=$HOME/.pyenv/bin:$PATH
-fi
-
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
 
