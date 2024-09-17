603713189caae0f758b1214a8f6d4ee834d07d3a
Matt Washer <github@mwasher.com>
2024-09-17 13:34:44 -0500

    Conditional to check for vscode

--- nvim/lua/mwasher/init.lua
+++ nvim/lua/mwasher/init.lua
@@ -8,20 +8,25 @@ local function load_plugin(item)
   table.insert(LAZY_PLUGINS, { import = item })
 end
 
-load_plugin('mwasher.plugins.colorscheme')
-load_plugin('mwasher.plugins.plenary')
-load_plugin('mwasher.plugins.nvim-web-devicons')
-load_plugin('mwasher.plugins.telescope')
-load_plugin('mwasher.plugins.telescope-file-browser')
-load_plugin('mwasher.plugins.nvim-tree')
-load_plugin('mwasher.plugins.lualine')
-load_plugin('mwasher.plugins.luatab')
-load_plugin('mwasher.plugins.treesitter')
-load_plugin('mwasher.plugins.comment')
-load_plugin('mwasher.plugins.schemastore')
-load_plugin('mwasher.plugins.mason')
-load_plugin('mwasher.plugins.lspconfig')
-load_plugin('mwasher.plugins.cmp')
-load_plugin('mwasher.plugins.none-ls')
+if vim.g.vscode then
+
+else
+  load_plugin('mwasher.plugins.colorscheme')
+  load_plugin('mwasher.plugins.plenary')
+  load_plugin('mwasher.plugins.nvim-web-devicons')
+  load_plugin('mwasher.plugins.telescope')
+  load_plugin('mwasher.plugins.telescope-file-browser')
+  load_plugin('mwasher.plugins.nvim-tree')
+  load_plugin('mwasher.plugins.lualine')
+  load_plugin('mwasher.plugins.luatab')
+  load_plugin('mwasher.plugins.treesitter')
+  load_plugin('mwasher.plugins.comment')
+  load_plugin('mwasher.plugins.schemastore')
+  load_plugin('mwasher.plugins.mason')
+  load_plugin('mwasher.plugins.lspconfig')
+  load_plugin('mwasher.plugins.cmp')
+  load_plugin('mwasher.plugins.none-ls')
+end
+
 require('mwasher.lazy')
 
