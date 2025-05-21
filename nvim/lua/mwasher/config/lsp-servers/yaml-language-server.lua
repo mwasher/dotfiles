b8eb1dbff6432bbacd6075f77ef128d30312e3b4
Matt Washer <git@mwasher.com>
2025-05-21 17:29:19 +0000

    Add k8s completions

--- nvim/lua/mwasher/config/lsp-servers/yaml-language-server.lua
+++ nvim/lua/mwasher/config/lsp-servers/yaml-language-server.lua
@@ -4,5 +4,17 @@ return {
 	root_markers = { ".git" },
 	settings = {
 		redhat = { telemetry = { enabled = false } },
+		yaml = {
+			schemas = {
+				kubernetes = "k8s-*.yaml",
+				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+				["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+			},
+		},
 	},
 }
